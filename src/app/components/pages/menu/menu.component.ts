eaebba049535d375762d98f97d05c9690f2b6e27
ELEOMAR <kaotikus27@gmail.com>
2024-11-02 17:24:40 +0800

    menu on progress

--- src/app/components/pages/menu/menu.component.ts
+++ src/app/components/pages/menu/menu.component.ts
@@ -22,6 +22,14 @@ export class MenuComponent implements OnInit  {
     
   }
 
+  /* PROTOTYPE TEST  */
+    espresso:boolean = true
+    nonCoffee:boolean = false
+    pasta:boolean = false
+    pastries:boolean = false
+    frappes:boolean = false
+    selectedList = 1;
+
   //set index of image on dot or indicator
   selectImage(index:number):void{
     this.selectedIndex = index;
@@ -45,6 +53,54 @@ export class MenuComponent implements OnInit  {
       this.selectedIndex++;
       console.log("s?")
     }
+  }
+
+
+
+
+  /* test test prototype */
+
+  showEspresso(){
+    this.espresso = true;
+    this.nonCoffee = false;
+    this.pasta = false;
+    this.pastries = false;
+    this.frappes = false;
+    this.selectedList = 1;
+  }
+
+  showNonCoffee(){
+    this.espresso = false;
+    this.nonCoffee = true;
+    this.pasta = false;
+    this.pastries = false;
+    this.frappes = false;
+    this.selectedList = 2;
+  }
+
+  showPasta(){
+    this.espresso = false;
+    this.nonCoffee = false;
+    this.pasta = true;
+    this.pastries = false;
+    this.frappes = false;
+    this.selectedList = 3;
+  }
+  showPastries(){
+    this.espresso = false;
+    this.nonCoffee = false;
+    this.pasta = false;
+    this.pastries = true;
+    this.frappes = false;
+    this.selectedList = 4;
+  }
+  showFrappes(){
+    this.espresso = false;
+    this.nonCoffee = false;
+    this.pasta = false;
+    this.pastries = false;
+    this.frappes = true;
+    this.selectedList = 5;
   }
 
 }
